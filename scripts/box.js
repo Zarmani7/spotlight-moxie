5f949ce80c033f06db5c79f98bf7743365468f72
Moses Ifechukwu Ekwugha <93547944+Zarmani7@users.noreply.github.com>
2022-10-19 10:20:48 -0700

    Add files via upload

---
+++ scripts/box.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $("nav ul").slideToggle(2000);
+    });
+});
+/*document.getElementById("text").onclick = function () {
+    alert("clicked");
+}*/
+
+
